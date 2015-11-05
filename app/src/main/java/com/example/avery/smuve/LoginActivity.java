f231bee32004ce3ec776e0afca93bf75adeb32cc
averyferrante <aferrante@smu.edu>
2015-11-04 22:51:06 -0600

    Initial commit

---
+++ app/src/main/java/com/example/avery/smuve/LoginActivity.java
@@ -0,0 +1,39 @@
+package com.example.avery.smuve;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    final private static String TAG = "1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_login);
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailView = (EditText) findViewById(R.id.email_input);
+                String email = emailView.getText().toString();
+
+                //CHECK IF EMAIL IS VALID INPUT
+            }
+        });
+    }
+
+
+}
